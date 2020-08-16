69238583c2bac775ff8256bda4da020ba46f20e6
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-08-16 21:49:22 +0900

    投稿詳細機能とそのテストをrequest specで実装

--- spec/api/v1/requests/articles_request_spec.rb
+++ spec/api/v1/requests/articles_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Articles", type: :request do
   describe "get /api/vi/articles.json" do
     subject { get(api_v1_articles_path) }
     before  { user_with_articles(articles_count: 3) }
-      fit "投稿一覧が取得できる" do
+      it "投稿一覧が取得できる" do
         subject
         res = JSON.parse(response.body)
 
@@ -13,5 +13,35 @@ RSpec.describe "Articles", type: :request do
         expect(res[0]["user"].keys).to eq ["id", "name", "email"]
         expect(response).to have_http_status(200)
       end
+  end
+
+  describe "get /api/vi/articles.json/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+    context "指定したidの投稿が取得できる時" do
+
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
+        fit "投稿詳細が取得できる" do
+
+          subject
+
+          res = JSON.parse(response.body)
+
+          expect(res["id"]).to eq article.id
+          expect(res["title"]).to eq article.title
+          expect(res["body"]).to eq article.body
+          expect(res["updated_at"]).to be_present
+          expect(res.keys).to eq ["id", "title", "body", "updated_at", "user",]
+          expect(res["user"].keys).to eq ["id", "name", "email"]
+          expect(response).to have_http_status(200)
+        end
+    end
+
+    context "指定したidの投稿が取得できる時" do
+      let(:article_id) { 10000 }
+        fit "投稿詳細が取得できない" do
+          expect{ subject }.to raise_error ActiveRecord::RecordNotFound
+        end
+    end
   end
 end
